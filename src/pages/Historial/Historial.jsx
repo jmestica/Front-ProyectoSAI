7e83d0121ee03345b6d6129e72c8048d6bd44b8c
Sebastian Antueno <pantueno@inti.gob.ar>
2023-10-26 17:25:48 -0300

    Se finaliza funcion historial de un reactivo

--- src/pages/Historial/Historial.jsx
+++ src/pages/Historial/Historial.jsx
@@ -5,54 +5,93 @@ const { Column, HeaderCell, Cell } = Table;
 import "./Historial.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { API_URL, PORT } from "../../../config";
 
 function Historial() {
   const params = useParams();
 
-  const [producto, setProducto] = useState(null)
+  const [infoReactivo, setInfoReactivo] = useState([])
   const [historial, setHistorial] = useState([]);
   const [datosAdicionales, setDatosAdicionales] = useState([]);
   const [datosFila, setDatosFila] = useState();
+  const [contador, setContador] = useState(1);
 
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+
+  // Llamada a una función asíncrona que devuelve un array de datos del historial
+  const fetchHistorial = async () => {
+
+    try {
+      const res = await axios.get(
+        `http://${API_URL}:${PORT}/api/reactivo/historial/${params.id}`
+      );
+
+      if (res.statusText !== "OK") {
+        toaster.push(errorConnection, { placement: "TopCenter" });
+        return;
+      }
+
+      // Asigna el n° de fila al contador y luego ++ el contador.
+      const historialData = res.data.map((item, index) => ({ ...item, index: contador + index }));
+      setHistorial(historialData);
+
+    } catch (error) {
+      console.error(error);
+      toaster.push(errorConnection, { placement: "TopCenter" });
+    }
+  }
+
+  // Llamada a una función asíncrona que devuelve la info de un reactivo.
+  const fetchReactivo = async () => {
+
+    try {
+      const res = await axios.get(
+        `http://${API_URL}:${PORT}/api/reactivo/${params.id}`
+      );
+
+      if (res.statusText !== "OK") {
+        toaster.push(errorConnection, { placement: "TopCenter" });
+        return;
+      }
+
+      const datosReactivo = res.data[0];
+      console.log(datosReactivo);
+      setInfoReactivo(datosReactivo)
+
+    } catch (error) {
+      console.error(error);
+      toaster.push(errorConnection, { placement: "TopCenter" });
+    }
+  };
+
+  const errorConnection = (
+    <Notification
+      header="Error al obtener los datos"
+      closable
+      type="error"
+    >
+      No se ha podido cargar los datos en este momento. Por favor, inténtalo de nuevo más tarde.
+    </Notification>
+  )
+
   useEffect(() => {
-    const fetchHistorial = async () => {
-      // Llamada a una función asíncrona que devuelve un array de datos del historial
-      const response = await axios.get(
-        `http://192.168.0.130:4000/api/pieza/historial/${params.id}`
-      )
-
-      const datosAdicionales = response.data.map(
-        (item) => item.datos_adicionales
-      )
-
-      const datosProducto =  await axios.get(
-        `http://192.168.0.130:4000/api/pieza/${params.id}`
-      )
-
-      setProducto(datosProducto.data)
-      setHistorial(response.data)
-      setDatosAdicionales(datosAdicionales)
-
-    
-
-
-    };
 
     fetchHistorial();
+    fetchReactivo();
+
   }, []);
 
-  const verDetalle = (rowData) => {
-    const index = rowData.numero_movimiento - 1;
-    const datosFila = datosAdicionales[index];
+  // const verDetalle = (rowData) => {
+  //   const index = rowData.id_consumo - 1;
+  //   const datosFila = datosAdicionales[index];
+  //   setDatosFila(datosFila);
 
-    setDatosFila(datosFila);
+  //   handleOpen();
+  // };
 
-    handleOpen();
-  };
 
   return (
     <div>
@@ -62,96 +101,55 @@ function Historial() {
         <h4 className="title-product">{params.id}</h4>
         <hr />
 
-        <div className="info-container">
+      <div className="info-container">
           <div className="info">
             <p className="subtitle">Nombre</p>
-            <p className="content">{producto?.nombre}</p>
+            <p className="content">{infoReactivo?.nombre_reactivo}</p>
           </div>
           <div className="info">
-            <p className="subtitle">Cantidad</p>
-            <p className="content">{producto?.cantidad} {producto?.unidad}</p>
+            <p className="subtitle">Cantidad (ml)</p>
+            <p className="content">{infoReactivo?.cantidad}</p>
           </div>
           <div className="info">
-            <p className="subtitle">Vendedor</p>
-            <p className="content">{producto?.nombre_vendedor}</p>
+            <p className="subtitle">Marca</p>
+            <p className="content">{infoReactivo?.marca}</p>
           </div>
           <div className="info">
-            <p className="subtitle">Fecha de Compra</p>
-            <p className="content">{producto?.fecha}</p>
+            <p className="subtitle">Fecha de Ingreso</p>
+            <p className="content">{infoReactivo?.fecha_ingreso}</p>
           </div>
           <div className="info">
-            <p className="subtitle">Monto</p>
-            <p className="content">${producto?.monto_compra}</p>
+            <p className="subtitle">Vencimiento</p>
+            <p className="content">{infoReactivo?.fecha_vto} </p>
           </div>
         </div>
 
-        <h4 className="title-product">Historial</h4>
+      <h4 className="title-product">Historial</h4>
 
-        <div className="table-container-lg">
-          <Table autoHeight cellBordered bordered data={historial} onRowClick={verDetalle}>
-            <Column align="center" width={35}>
-              <HeaderCell>#</HeaderCell>
-              <Cell dataKey="numero_movimiento" fullText />
-            </Column>
-            <Column align="center" fixed flexGrow={1}>
-              <HeaderCell>Movimiento</HeaderCell>
-              <Cell dataKey="accion" />
-            </Column>
-            <Column align="center" fixed width={100}>
-              <HeaderCell>Fecha</HeaderCell>
-              <Cell dataKey="fecha" />
-            </Column>
-            <Column align="center" fixed width={70}>
-              <HeaderCell>Hora</HeaderCell>
-              <Cell dataKey="hora" />
-            </Column>
-            <Column align="center" fixed width={100}>
-              <HeaderCell>Usuario</HeaderCell>
-              <Cell dataKey="nombre_usuario" />
-            </Column>
-          </Table>
-        </div>
-
-        <div className="table-container-sm">
-          <Table
-            autoHeight
-            cellBordered
-            bordered
-            data={historial}
-            onRowClick={verDetalle}
-          >
-            <Column align="center" fixed flexGrow={1} fullText>
-              <HeaderCell>Movimiento</HeaderCell>
-              <Cell dataKey="accion" />
-            </Column>
-            <Column align="center" fixed width={90}>
-              <HeaderCell>Fecha</HeaderCell>
-              <Cell dataKey="fecha" />
-            </Column>
-            <Column align="center" fixed width={60}>
-              <HeaderCell>Hora</HeaderCell>
-              <Cell dataKey="hora" />
-            </Column>
-            <Column align="center" fixed width={100}>
-              <HeaderCell>Usuario</HeaderCell>
-              <Cell dataKey="nombre_usuario" />
-            </Column>
-          </Table>
-        </div>
-
-        <Modal open={open} onClose={handleClose}>
-          <Modal.Header>
-            <Modal.Title>Datos adicionales</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>{datosFila}</Modal.Body>
-          <Modal.Footer>
-            <Button onClick={handleClose} block appearance="primary">
-              Cerrar
-            </Button>
-          </Modal.Footer>
-        </Modal>
+      <div className="table-container-lg">
+        <Table autoHeight cellBordered bordered data={historial}>
+          <Column align="center" width={35}>
+            <HeaderCell>#</HeaderCell>
+            <Cell dataKey="index" fullText />
+          </Column>
+          <Column align="center" fixed flexGrow={1}>
+            <HeaderCell>Consumo (ml)</HeaderCell>
+            <Cell dataKey="cantidad_usada" />
+          </Column>
+          <Column align="center" fixed flexGrow={2}>
+            <HeaderCell>Fecha</HeaderCell>
+            <Cell dataKey="registro_consumo" />
+          </Column>
+          <Column align="center" fixed flexGrow={1}>
+            <HeaderCell>Usuario</HeaderCell>
+            <Cell dataKey="nombre_usuario" />
+          </Column>
+        </Table>
       </div>
+
+      
     </div>
+    </div >
   );
 }
 
