546185870d7e4aa7b0784d3f781559f41a068591
Sebastian Antueno <pantueno@inti.gob.ar>
2023-10-31 14:53:55 -0300

    Se implementa funcionalidad de control de stock

--- src/pages/ConsultaStock/ConsultaStock.jsx
+++ src/pages/ConsultaStock/ConsultaStock.jsx
@@ -1,77 +1,221 @@
-import { SelectPicker, Table } from 'rsuite';
-const { Column, HeaderCell, Cell } = Table;
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import { Form, Table, SelectPicker, InputPicker, Button, Loader, Placeholder, Notification, useToaster } from 'rsuite';
+
 
 import TopBar from "../../components/TopBar/TopBar";
 import './ConsultaStock.css'
+import { API_URL, PORT } from '../../../config';
+
+const { Column, HeaderCell, Cell } = Table;
+
 
 const laboratorio = ['Cromatografía'].map(
-    item => ({ label: item, value: item })
-  );
+  item => ({ label: item, value: item })
+);
 
 const nombres_reactivos = ['Acetona Grado Plaguicida', 'Acetona PA', 'Cloruro de Metileno Grado Plaguicida - Diclorometano', 'Éter de Petróleo Grado Plaguicida', 'Sulfato de Sodio Anhidro', 'Hexano Grado Plaguicida', 'Metanol Grado Plaguicida'].map(
-    item => ({ label: item, value: item })
-  );
+  item => ({ label: item, value: item })
+);
 
-const stock = ['En Stock', 'Fuera de Stock','Descartado', 'Vencido'].map(
-    item => ({ label: item, value: item })
-  );
+const stock = ['En Stock', 'Sin Stock', 'Descartado', 'Vencido'].map(
+  item => ({ label: item, value: item })
+);
 
 function ConsultaStock() {
+
+  const [tableData, setTableData] = useState([]);
+  const [labFilter, setLabFilter] = useState('');
+  const [tipoFilter, setTipoFilter] = useState('');
+  const [stockFilter, setStockFilter] = useState('');
+  const [contador, setContador] = useState(1);
+  const [loading, setLoading] = useState(false)
+
+  const toaster = useToaster();
+
+  const errorConnection = (
+    <Notification
+      header="Error al obtener los datos"
+      closable
+      type="error"
+    >
+      No se ha podido cargar los datos en este momento. Por favor, intente nuevamente más tarde.
+    </Notification>
+  );
+
+  const errorNotFound = (
+    <Notification
+      header="Sin resultados"
+      closable
+      type="info"
+    >
+      No se encontraron reactivos que cumplan con los filtros seleccionados.
+    </Notification>
+  );
+
+  const errorNotEmpty = (
+    <Notification
+      header="Atención"
+      closable
+      type="warning"
+    >
+      Los filtros son requeridos para realizar una búsqueda. Por favor seleccione las opciones disponibles.
+    </Notification>
+  );
+
+
+  /* FX QUE TRAE TODOS LOS REACTIVOS */
+
+  const handleSubmit = async (e) => {
+
+    if (labFilter == "" || tipoFilter == "" || stockFilter == "") {
+      toaster.push(errorNotEmpty, { placement: 'topCenter' });
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+
+      const res = await axios.get(`http://${API_URL}:${PORT}/api/reactivo/getFiltrados?labFilter=${labFilter}&tipoFilter=${tipoFilter}&stockFilter=${stockFilter}`);
+
+      if (res.statusText !== "OK") {
+
+        toaster.push(errorConnection, { placement: 'topCenter' });
+        setLoading(false)
+        return;
+      }
+
+      // cuando no se encuentran resultados
+      if (res.data.status == 400) {
+        toaster.push(errorNotFound, { placement: 'topCenter' });
+        setTableData([])
+        setLoading(false);
+        return;
+      }
+
+      const filteredData = res.data.map((item, index) => ({
+        ...item,
+        index: contador + index,
+        fechaIngreso: new Date(`${item.fecha_ingreso}`).toLocaleDateString(),
+        fechaVto: new Date(`${item.fecha_vto}`).toLocaleDateString()
+      }));
+
+      setTableData(filteredData);
+      setLoading(false)
+
+    }
+    catch (error) {
+      console.error("Error al obtener los reactivos. Intente nuevamente...", error);
+      toaster.push(errorConnection, { placement: 'topCenter' });
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+
+  }, [tableData]);
+
+
   return (
+
     <div>
       <TopBar />
       <div className="section">
         <h3 className="section-title"> Consulta de Stock </h3>
         <p className="desc">
-            Utilizá los filtros para conocer stock y próximos vencimientos. Hacé click sobre las filas para ver el detalle de cada reactivo
+          Utilizá los filtros para conocer stock y próximos vencimientos. Hacé click sobre las filas para ver el detalle de cada reactivo
         </p>
+        <br />
 
-    <div className="filter-container">
-        <SelectPicker  data={laboratorio} searchable={false} placeholder="Selecciona el laboratorio"/>
-        <SelectPicker placement='bottom' id='mid'  data={nombres_reactivos} placeholder="Nombre de Reactivo" style={{margin: '0 30px'}}/>
-        <SelectPicker  data={stock} placeholder="Seleccione la condición del reactivo" searchable={false}/>
-    </div>
+        <Form onSubmit={handleSubmit}>
+          <div className="filter-container">
+            <InputPicker
+              placeholder="Filtrar por laboratorio"
+              data={laboratorio}
+              value={labFilter}
+              onChange={(value) => setLabFilter(value)}
+              disabled={loading}
+              required
+            />
+            <InputPicker
+              placeholder="Filtrar por tipo de reactivo"
+              data={nombres_reactivos}
+              value={tipoFilter}
+              onChange={(value) => setTipoFilter(value)}
+              disabled={loading}
+              required
+            />
+            <SelectPicker
+              data={stock}
+              placeholder="Filtrar por stock"
+              value={stockFilter}
+              onChange={(value) => setStockFilter(value)}
+              searchable={false}
+              disabled={loading}
+              required
+            />
 
-    <Table virtualized height={400} data={[{}]}>
-      <Column width={70} align="center" fixed>
-        <HeaderCell>Id</HeaderCell>
-        <Cell dataKey="id" />
-      </Column>
+            <br />
 
-      <Column width={130}>
-        <HeaderCell>First Name</HeaderCell>
-        <Cell dataKey="firstName" />
-      </Column>
+            <Button style={{ margin: "10px" }} appearance="primary" type="submit" disabled={loading}>
+              Buscar
+            </Button>
+          </div>
 
-      <Column width={130}>
-        <HeaderCell>Last Name</HeaderCell>
-        <Cell dataKey="lastName" />
-      </Column>
+        </Form>
 
-      <Column width={100}>
-        <HeaderCell>Gender</HeaderCell>
-        <Cell dataKey="gender" />
-      </Column>
+        {
+          loading
+            ? (
+              <div>
+                <Placeholder.Paragraph rows={8} />
+                <Loader backdrop content="Buscando los reactivos, aguarde por favor..." vertical />
+              </div>
+            ) : null
+        }
 
-      <Column width={100}>
-        <HeaderCell>Age</HeaderCell>
-        <Cell dataKey="age" />
-      </Column>
+        <div className="table-container-stock">
+          <Table autoHeight cellBordered bordered data={tableData}>
+            <Column align="center" width={38}>
+              <HeaderCell>#</HeaderCell>
+              <Cell dataKey="index" />
+            </Column>
+            <Column align="center" width={100}>
+              <HeaderCell>Código</HeaderCell>
+              <Cell dataKey="codigo" />
+            </Column>
+            <Column align="center" width={150}>
+              <HeaderCell>Tipo</HeaderCell>
+              <Cell dataKey="nombre_reactivo" />
+            </Column>
+            <Column align="center" width={100}>
+              <HeaderCell>Ingreso</HeaderCell>
+              <Cell dataKey="fechaIngreso" />
+            </Column>
+            <Column align="center" width={100}>
+              <HeaderCell>Vencimiento</HeaderCell>
+              <Cell dataKey="fechaVto" />
+            </Column>
+            <Column align="center" width={100}>
+              <HeaderCell>N° Lote</HeaderCell>
+              <Cell dataKey="nro_lote" />
+            </Column>
+            <Column align="center" width={100}>
+              <HeaderCell>N° Expediente</HeaderCell>
+              <Cell dataKey="nro_expediente" />
+            </Column>
+            <Column align="center" width={100}>
+              <HeaderCell>Cantidad</HeaderCell>
+              <Cell>
+                {rowData => rowData.cantidad + ' ml'}
+              </Cell>
+            </Column>
+          </Table>
+        </div>
 
-      <Column width={200}>
-        <HeaderCell>City</HeaderCell>
-        <Cell dataKey="city" />
-      </Column>
-
-      <Column width={200}>
-        <HeaderCell>Email</HeaderCell>
-        <Cell dataKey="email" />
-      </Column>
-    </Table>
-
-
-  
       </div>
+
     </div>
   );
 }
