0ed0d2c3423db933756e7d8d1bce6ab657f76457
jmestica <jmestica@inti.gob.ar>
2023-11-02 14:45:00 -0300

    correcciones en los códigos de reactivo

--- src/pages/EditarReactivo/EditarReactivo.jsx
+++ src/pages/EditarReactivo/EditarReactivo.jsx
@@ -1,6 +1,4 @@
 import TopBar from '../../components/TopBar/TopBar'
-import Card from '../../components/Cards/Card'
-import {Row, Col} from 'rsuite'
 import './EditarReactivo.css'
 
 function EditarReactivo() {
